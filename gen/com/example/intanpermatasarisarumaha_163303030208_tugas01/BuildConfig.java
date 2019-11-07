963c99371c7ed66b18a6068ebf64b564afd083fa
intansarumaha <permatasarisarumaha99@gmail.com>
2019-11-07 13:49:11 +0700

    Tugas 1 Pemograman Mobile II

---
+++ gen/com/example/intanpermatasarisarumaha_163303030208_tugas01/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.intanpermatasarisarumaha_163303030208_tugas01;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
